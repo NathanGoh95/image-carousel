2c1e3043dee6c6f7c512aeb462df082b4b399e65
NathanGoh95 <gohjunhao95@gmail.com>
2025-02-03 16:24:24 +0800

    Replace switch with button toggle, fix mode change bg color, modify button component

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,14 +1,17 @@
+import { Moon, Sun } from 'lucide-react';
 import { useTheme } from './ThemeProvider';
-import { Switch } from './ui/switch';
+import { Button } from './ui/button';
 
 export const ThemeSwitch = () => {
   const { theme, setTheme } = useTheme();
 
-  const handleSwtich = () => {
+  const handleThemeToggle = () => {
     setTheme(theme === 'dark' ? 'light' : 'dark');
   };
 
   return (
-    <Switch checked={theme === 'dark'} onCheckedChange={handleSwtich} aria-label='Switch theme' />
-  )
+    <Button variant={'outline'} size={'icon'} onClick={handleThemeToggle} aria-label='Swtich Theme' className='p-2 rounded-md transition-colors'>
+      {theme === 'dark' ? <Moon /> : <Sun />}
+    </Button>
+  );
 };
